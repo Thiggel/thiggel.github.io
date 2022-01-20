bb02162b6bdf2fd26bbe730a6d490cf67604048e
Filipe Laitenberger <f.laitenberger@gmail.com>
2022-01-20 17:48:48 +0100

    deploy

---
+++ assets/Apartment.4999cd94.js
@@ -0,0 +1,1 @@
+import{r,o as e,c as t,w as o,a as _,T as s,F as c}from"./vendor.78f108d8.js";import{_ as i}from"./index.cbeba2f2.js";const m={};function d(f,l){const a=r("router-view");return e(),t(a,null,{default:o(({Component:n})=>[_(s,{name:"fade",mode:"out-in"},{default:o(()=>[(e(),t(c(n)))]),_:2},1024)]),_:1})}var v=i(m,[["render",d]]);export{v as default};
